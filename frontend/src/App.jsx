c23c6f534902765151788e2315bfdf793186533a
paulo975 <paulo@prisco-gestao.com>
2026-06-29 06:20:09 +0100

    Add Toaster component to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DSPApp from "@/pages/DSPApp";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
@@ -11,9 +12,12 @@ function App() {
           <Route path="/" element={<DSPApp />} />
           <Route path="*" element={<DSPApp />} />
         </Routes>
+        <Toaster />
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
